651ec95d57cd42f8fb21921b3e49fa27bbc920fc
Gogeishvili <gio.gogeishvili@gmail.com>
2024-11-10 19:24:46 +0400

    fixed small bug

--- shop_project/store/views.py
+++ shop_project/store/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from . import models
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework import permissions
 
 
 def products_JSON_view(request):
@@ -20,6 +20,7 @@ def tags_JSON_view(request):
     return JsonResponse({"tags": tags})
 
 @api_view(['GET','POST'])
+@permission_classes([permissions.IsAuthenticated])
 def products(request):
     print(request.method)
     products_data_list = models.Product.objects.get_prodacts_data_list()
